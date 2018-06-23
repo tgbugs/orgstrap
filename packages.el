05dd1e896bb43331a955064991f93e81c3c1e846
Tom Gillespie <tgbugs@gmail.com>
2018-06-23 03:33:03 -0700

    packages org added new link format img to manage pandoc issues
    pandoc converts <a href><img src></> into [[href][[[src]]]] which
    makes sense, but org doesn't know how to deal with that and chokes

--- packages.el
+++ packages.el
@@ -7,7 +7,15 @@
 
 ;; * org-mode
 (use-package org
-             :mode ("\\.org\\'" . org-mode))
+             :mode ("\\.org\\'" . org-mode)
+             :config
+             (defun org-custom-link-img-follow (path)
+               (org-open-link-from-string path))
+             (defun org-custom-link-img-export (path desc format)
+               (cond ((eq format 'html)
+                      (format "<a href=\"%s\"><img src=\"%s\"/></a>" desc path))))
+             ; TODO figure out how to suppress the message here
+             (org-add-link-type "img" 'org-custom-link-img-follow 'org-custom-link-img-export))
 
 (use-package htmlize)
 
