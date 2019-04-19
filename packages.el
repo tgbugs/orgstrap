14f1459cea907b91b3f15db8d356c74534b9de8a
Tom Gillespie <tgbugs@gmail.com>
2019-04-19 01:23:36 -0700

    packages.el org add custom link to handle absolute local hrefs

--- packages.el
+++ packages.el
@@ -7,16 +7,26 @@
 
 ;; * org-mode
 (use-package org
-             :mode ("\\.org\\'" . org-mode)
-             :config
-			 (setq org-export-with-broken-links 1)  ; needed on some systems
-             (defun org-custom-link-img-follow (path)
-               (org-open-link-from-string path))
-             (defun org-custom-link-img-export (path desc format)
-               (cond ((eq format 'html)
-                      (format "<a href=\"%s\"><img src=\"%s\"/></a>" desc path))))
-             ; TODO figure out how to suppress the message here
-             (org-add-link-type "img" 'org-custom-link-img-follow 'org-custom-link-img-export))
+  :mode ("\\.org\\'" . org-mode)
+  :config
+  (setq org-export-with-broken-links 1)  ; needed on some systems
+
+  (defun org-custom-link-img-follow (path)
+    (org-open-link-from-string path))
+  (defun org-custom-link-img-export (path desc format)
+    (cond ((eq format 'html)
+           (format "<a href=\"%s\"><img src=\"%s\"/></a>" desc path))))
+
+  ;; TODO figure out how to suppress the message here
+  (org-add-link-type "img" 'org-custom-link-img-follow 'org-custom-link-img-export)
+
+  (defun org-custom-hrefl (path)
+    "do nothing because we don't know the context of the export")
+  (defun org-custom-hrefl-export (path desc format)
+    "export a rooted path as a an absolute href with no server"
+    (cond ((eq format 'html)
+           (format "<a href=\"%s\">%s</a>" path desc))))
+  (org-add-link-type "hrefl" 'org-custom-hrefl 'org-custom-hrefl-export))
 
 (use-package htmlize)
 
