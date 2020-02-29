d521e3aa3bd6f916f1395011e1dc19866c78d8ee
Tom Gillespie <tgbugs@gmail.com>
2020-02-28 21:14:31 -0800

    packages.el move the custom links fuctions and custom variables out
    this was never really an appriopriate place for those functions to
    live, now that docs-init.ed exists in pyontutils/nifstd/resources
    there is a more natural place to keep those, allowing orgstrap to be
    as minimal as possible

--- packages.el
+++ packages.el
@@ -7,36 +7,7 @@
 
 ;; * org-mode
 (use-package org-plus-contrib
-  :mode ("\\.org\\'" . org-mode)
-  :custom
-  (org-export-with-broken-links t "needed for hrefl among others")
-  (org-src-fontify-natively t)
-  :init
-  (require 'ol)  ; must require otherwise custom parameters wont stick
-
-  ;; img tag links
-  ;; NOTE these are for nested images inside other links NOT for images
-  (defun org-custom-link-img (path)
-    (org-link-open-from-string path))
-  (defun org-custom-link-img-export (path desc format)
-    (cond ((eq format 'html)
-           (format "<a href=\"%s\"><img src=\"%s\"/></a>" desc path))))
-
-  ;; TODO figure out how to suppress the message here
-  (org-link-set-parameters "img"
-                           :follow #'org-custom-link-img
-                           :export #'org-custom-link-img-export)
-
-  ;; server local hrefs
-  (defun org-custom-hrefl (path)
-    "do nothing because we don't know the context of the export")
-  (defun org-custom-hrefl-export (path desc format)
-    "export a rooted path as an absolute href with no server"
-    (cond ((eq format 'html)
-           (format "<a href=\"%s\">%s</a>" path desc))))
-  (org-link-set-parameters "hrefl"
-                           :follow #'org-custom-hrefl
-                           :export #'org-custom-hrefl-export))
+  :mode ("\\.org\\'" . org-mode))
 
 (use-package htmlize)
 
