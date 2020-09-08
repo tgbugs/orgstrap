590427afcc24bc31ea9adbc4cd1858c558f6e178
Tom Gillespie <tgbugs@gmail.com>
2020-09-07 19:44:46 -0700

    specification, orgstrap.el description, prose cleanup
    the spec is fairly stright forward, 3 prop line vars, how
    implementations know they are correct, and how to register
    normalization functions, and that's about it, my portable
    implementation using the local variables list has a number
    of nice features, but none of it is actually required for
    the spec, there are just tradeoffs, and not all impls have
    to do things like embed the checksum in the prop line
    
    removed historical local variables since the normalization functions
    are what needs to be and will be preserved without changes
    
    also it seems that somewhere along the line the issue with losing
    before-save-hook was fixed, so there are no known issues :D!
    
    updated the status of various future directions

--- orgstrap.el
+++ orgstrap.el
@@ -13,18 +13,31 @@
 
 ;;; Commentary:
 
-;; The license for the orgstrap.el code reflects the fact that
-;; `orgstrap-get-block-checksum' reuses code from
-;; `org-babel-check-confirm-evaluate' which is
-;; (at the time of writing) in ob-core.el and licensed
-;; as part of Emacs.
+;; orgstrap is a specification and tooling for bootstrapping Org files.
+
+;; It allows Org files to describe their own requirements, and
+;; define their own functionality, making them self-contained,
+;; standalone computational artifacts, dependent only on Emacs,
+;; or other implementations of the Org-babel protocol in the future.
+
+;; orgstrap.el is an elisp implementation of the orgstrap conventions and
+;; provides `orgstrap-init' and `orgstrap-mode' to simplify authoring of
+;; orgstrapped files. For more details see README.org which is also the
+;; literate source for this orgstrap.el file in the git repo at
+;; https://github.com/tgbugs/orgstrap/blob/master/README.org
+;; or whever you can find git:c1b28526ef9931654b72dff559da2205feb87f75
 
 ;; Code in an orgstrap block is usually meant to be executed directly by its
-;; containing org file.  However, if the code is something that will be reused
-;; over time outside the defining org file then it may be better to tangle and
-;; load the file so that it is easier to debug/xref functions.  This code in
-;; particular is tangled for inclusion in one of the *elpas so as to protect
-;; the orgstrap namespace.
+;; containing Org file.  However, if the code is something that will be reused
+;; over time outside the defining Org file, then it may be better to tangle and
+;; load the file so that it is easier to debug/xref functions.  The code in
+;; this orgstrap.el file in particular is tangled for inclusion in one of the
+;; *elpas so as to protect the orgstrap namespace and to make it eaiser to
+;; use orgstrap in Emacs.
+
+;; The license for the orgstrap.el code reflects the fact that the
+;; code for expanding and hashing blocks reuses code from ob-core.el,
+;; which at the time of writing is licensed as part of Emacs.
 
 ;;; Code:
 
